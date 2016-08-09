1d55cf8fb2f3e5fd1b03c324d50b8462a34c08cd
Melanie & Will Friebel <pair+melaniesarah.wfriebel@devbootcamp.com>
2016-08-09 15:32:30 -0700

    Add associations and controllers

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,9 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160809192909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "votes"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
