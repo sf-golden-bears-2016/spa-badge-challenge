29b9e2e3de782cf989020da53e1b0986fbaa620b
Alexander Pellas & Jason Sutter <pair+alexpadraic.jasonsutter87@devbootcamp.com>
2016-08-09 11:08:43 -0700

    Initial commit.

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+  Teacher.create(name: Faker::Name.name)
+end
+
+
+50.times do
+  Badge.create(content: Faker::Team.name, user_id: rand(1..10), vote_count: rand(0..100))
+end
