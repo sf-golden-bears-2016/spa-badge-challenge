9947e057ae854be904dc6021f6fa5557d8820d27
Carrie Liang <20.c.liang@gmail.com>
2016-08-09 17:49:48 -0700

    used rack cors gem to decouple

---
+++ api/db/migrate/20160809222318_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer :badge_id
+      t.integer :value, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
