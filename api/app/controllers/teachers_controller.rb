b1ac1ca39660b4148fc5fb88baf398fc90579494
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-10 12:14:38 -0700

    display badges on teacher page

--- api/app/controllers/teachers_controller.rb
+++ api/app/controllers/teachers_controller.rb
@@ -7,6 +7,11 @@ class TeachersController < ApplicationController
 
   def show
     @teacher = Teacher.find(params[:id])
+    # post.to_json(:include => :user => {:only => :username })
+    headers['Access-Control-Allow-Origin'] = '*'
+    json_data = { teacher: @teacher, badges: @teacher.badges}
+    render :json => json_data
+    # render :json => @teacher
   end
 
 end
